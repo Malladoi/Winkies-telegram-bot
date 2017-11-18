3a3dd6693c9c1b94f0465ca43023788c52c44b67
Malladoi <iloveyoubutyoudontcare@gmail.com>
2017-11-18 13:12:54 +0300

    fix deploy from github

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Winkies',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Thebluebox',
+    author_email='',
+    description=''
+)
