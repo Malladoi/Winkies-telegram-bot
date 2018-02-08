a6117ae5b7e8704a7ab546aa6e2592b7a1b20a23
Malladoi <iloveyoubutyoudontcare@gmail.com>
2018-02-08 23:13:24 +0300

    test

--- main.py
+++ main.py
@@ -29,12 +29,13 @@ bot = telebot.AsyncTeleBot(token + ":" + api_token)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    cur.execute("""
-        CREATE TABLE vendors (
-            vendor_id SERIAL PRIMARY KEY,
-            vendor_name VARCHAR(255) NOT NULL
-        )
-        """)
+    # cur.execute("""
+    #     CREATE TABLE vendors (
+    #         vendor_id SERIAL PRIMARY KEY,
+    #         vendor_name VARCHAR(255) NOT NULL
+    #     )
+    #     """)
+    cur.execute("""insert into vendors(vendor_id, vendor_name) values(1, 'Apple')""")
     # test = cur.fetchall()[1]
     cur.close()
     # commit the changes
