e3e425a9b20c9079718fb665f876debabdff60a4
Malladoi <iloveyoubutyoudontcare@gmail.com>
2018-02-10 18:41:27 +0300

    key

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ def send_welcome(message):
     itembtn2 = types.KeyboardButton('v')
     itembtn3 = types.KeyboardButton('d')
     markup.add(itembtn1, itembtn2, itembtn3)
-    bot.send_message(message.chat_id, "Choose one letter:", reply_markup=markup)
+    bot.send_message(message.id, "Choose one letter:", reply_markup=markup)
+    
     result = task.wait()
     print(result)
 
