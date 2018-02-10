c83defb1ed3798c60803ff1e8ed950c405fb1142
Malladoi <iloveyoubutyoudontcare@gmail.com>
2018-02-10 18:33:54 +0300

    keys

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import logging
 import os
 import telebot
 import databaseAPI
+from telebot import types
 from urllib import parse
 
 parse.uses_netloc.append("postgres")
@@ -26,16 +27,20 @@ bot = telebot.AsyncTeleBot(token + ":" + api_token)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    # task = bot.reply_to(message, "Welcome to Winkie's restaurant on Sunset Blvd")
-    task = bot.reply_to(message, databaseAPI.getserverversion(conn))
-    # bot.reply_to(message, res)
+    task = bot.reply_to(message, "")
+    task = bot.reply_to(message, "Привет, я бот mhelp.by")
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    itembtn1 = types.KeyboardButton('a')
+    itembtn2 = types.KeyboardButton('v')
+    itembtn3 = types.KeyboardButton('d')
+    markup.add(itembtn1, itembtn2, itembtn3)
+    bot.send_message(message.chat, "Choose one letter:", reply_markup=markup)
     result = task.wait()
     print(result)
 
 
 @bot.message_handler(commands=['recreatedb'])
 def send_welcome(message):
-    # task = bot.reply_to(message, "Welcome to Winkie's restaurant on Sunset Blvd")
     task = bot.reply_to(message, databaseAPI.recreatedb(conn))
     # bot.reply_to(message, res)
     result = task.wait()
